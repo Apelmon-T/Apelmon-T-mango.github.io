218173b25cd1cfa95ebfb4fd4ff72489b8d5a82a
Apelmon-T <60505107+Apelmon-T@users.noreply.github.com>
2020-07-16 17:04:48 +0300

    Add files via upload

---
+++ precache-manifest.b1bcf74bea594654a56df5aa9d18c49f.js
@@ -0,0 +1,138 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "aef566ffd3e9aee3774fe090d3600c9b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ba36626919ea697ca972",
+    "url": "/static/css/main.26f66c99.chunk.css"
+  },
+  {
+    "revision": "d5f0f57aa27456da37d4",
+    "url": "/static/js/2.f41e61ff.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.f41e61ff.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ba36626919ea697ca972",
+    "url": "/static/js/main.45de1fdd.chunk.js"
+  },
+  {
+    "revision": "bd53f6094fab913e0cc4",
+    "url": "/static/js/runtime-main.d6aec1e8.js"
+  },
+  {
+    "revision": "b324df5fbd798481187fbd7636311ad4",
+    "url": "/static/media/arrowLeft.b324df5f.svg"
+  },
+  {
+    "revision": "a9be266979c872392de7edc1d2d4eb70",
+    "url": "/static/media/arrowRight.a9be2669.svg"
+  },
+  {
+    "revision": "2a2bb5647a3c5481b6d103243c4eb1f2",
+    "url": "/static/media/background.2a2bb564.svg"
+  },
+  {
+    "revision": "826bc9c3de7a4ed5c8b5c41a8a75f9e4",
+    "url": "/static/media/background.826bc9c3.svg"
+  },
+  {
+    "revision": "591555344ef782607ddba2dd4783bc67",
+    "url": "/static/media/bottle.59155534.svg"
+  },
+  {
+    "revision": "5cfdf4c419b38fbf8c28fbd114e12590",
+    "url": "/static/media/burger.5cfdf4c4.svg"
+  },
+  {
+    "revision": "32d1d8ebdd5e968dd2a013877e1fdf2f",
+    "url": "/static/media/glassOne.32d1d8eb.svg"
+  },
+  {
+    "revision": "8e5ae1d61e7172fa1fdde1dacf893323",
+    "url": "/static/media/glassTwo.8e5ae1d6.svg"
+  },
+  {
+    "revision": "28fb0bbc5c2b1c5e94b3d139d8d773e4",
+    "url": "/static/media/grape.28fb0bbc.svg"
+  },
+  {
+    "revision": "1c6b522f90d313093cb38ee8cc3efd67",
+    "url": "/static/media/grapeFour.1c6b522f.svg"
+  },
+  {
+    "revision": "279e65f6212a8dd168b45ad72b7a4442",
+    "url": "/static/media/grapeIntroOne.279e65f6.svg"
+  },
+  {
+    "revision": "8dd9acda279da8e0b59531e3e1cdd3c2",
+    "url": "/static/media/grapeIntroTwo.8dd9acda.svg"
+  },
+  {
+    "revision": "35f7874ac9f275b7c43487e27b839689",
+    "url": "/static/media/grapeLeaf.35f7874a.svg"
+  },
+  {
+    "revision": "44b752cc79c703b5abf7aff5bd071a26",
+    "url": "/static/media/grapeLeaf.44b752cc.svg"
+  },
+  {
+    "revision": "82a28e5709bbac32d9d6d0cfa497605d",
+    "url": "/static/media/grapeLeafSecond.82a28e57.svg"
+  },
+  {
+    "revision": "646addb1f31045afb49f937e1fbc7820",
+    "url": "/static/media/grapeOne.646addb1.svg"
+  },
+  {
+    "revision": "c62d952f48ed669107477e984ca6f4d7",
+    "url": "/static/media/grapeOne.c62d952f.svg"
+  },
+  {
+    "revision": "a4321a27d3bacb0de388c9b7402dc175",
+    "url": "/static/media/grapeThree.a4321a27.svg"
+  },
+  {
+    "revision": "3b7a0009ef1d4ed5764bbbb471c6786b",
+    "url": "/static/media/grapeTwo.3b7a0009.svg"
+  },
+  {
+    "revision": "545b67ac392de6304da8f7925e9bed2d",
+    "url": "/static/media/grapeTwo.545b67ac.svg"
+  },
+  {
+    "revision": "bf6485f0660ca3639baad67643ab7f4e",
+    "url": "/static/media/introGrape.bf6485f0.svg"
+  },
+  {
+    "revision": "847a3b084759a7c075daedffac3b25d2",
+    "url": "/static/media/logo.847a3b08.svg"
+  },
+  {
+    "revision": "43f1108b044c7851f358aae681a79ce9",
+    "url": "/static/media/logoForBurger.43f1108b.svg"
+  },
+  {
+    "revision": "7d62e60cbfb1cee11c1e0e8e319bc891",
+    "url": "/static/media/modalFile.7d62e60c.svg"
+  },
+  {
+    "revision": "7800ac18514e93476918d16966a9e093",
+    "url": "/static/media/sliderCenter.7800ac18.svg"
+  },
+  {
+    "revision": "5fa52d0b8b31bac45133ce6b5ac1f5ac",
+    "url": "/static/media/sliderLeft.5fa52d0b.svg"
+  },
+  {
+    "revision": "7f91235552eed0b2735f30ac0a309903",
+    "url": "/static/media/sliderRight.7f912355.svg"
+  },
+  {
+    "revision": "b9396d675d6cfdd41039c8cb5f59f5f2",
+    "url": "/static/media/vector.b9396d67.svg"
+  }
+]);
